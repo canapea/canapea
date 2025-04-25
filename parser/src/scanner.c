54195e7b9943df945d3f71067d886a4f8a05a9cd
Martin Feineis <mfeineis@users.noreply.github.com>
2025-04-26 01:04:24 +0200

    refactor: Cleanup scanner.c cruft (#3)
    Programs with regular indentation work well right now. Let's hope we don't need to touch the block detection logic anymore.

--- parser/src/scanner.c
+++ parser/src/scanner.c
@@ -9,8 +9,6 @@
 #include "tree_sitter/array.h"
 
 #include <assert.h>
-// #include <ctype.h>
-#include <string.h>
 
 // TODO: For perf it'd be advantageous, if we could just memcpy the whole state
 typedef struct {
@@ -129,10 +127,7 @@ static inline void skip(TSLexer* lexer) { lexer->advance(lexer, true); }
 
 enum TokenType {
     IMPLICIT_BLOCK_OPEN,
-    // IMPLICIT_EMPTY_BLOCK,
     IMPLICIT_BLOCK_CLOSE,
-    // TOPLEVEL_RESET,
-    // TERMINATOR,
     IS_IN_ERROR_RECOVERY,
 };
 
@@ -159,7 +154,6 @@ static bool scan(Scanner* scanner, TSLexer* lexer, const bool* valid_symbols) {
 
     // Check if we have newlines and how much indentation
     bool has_newline = false;
-    // bool immediate_newline = lexer->lookahead == '\n';
 
     // newline_search:
     while (true) {
@@ -175,60 +169,11 @@ static bool scan(Scanner* scanner, TSLexer* lexer, const bool* valid_symbols) {
                 if (lexer->lookahead == ' ') {
                     skip(lexer);
                 }
-                // else if (valid_symbols[IMPLICIT_BLOCK_CLOSE]
-                //     && lexer->eof(lexer)
-                // ) {
-                //     scanner->indent_length = lexer->get_column(lexer);
-                //     lexer->result_symbol = IMPLICIT_BLOCK_CLOSE;
-                //     return true;
-                // }
                 else {
-                    // if (valid_symbols[IMPLICIT_BLOCK_CLOSE]
-                    //     && (lexer->lookahead == '{' || lexer->lookahead == '.')
-                    // ) {
-                    //     return false;
-                    // }
                     scanner->indent_length = lexer->get_column(lexer);
                     break; // column_search;
                 }
             }
-        }
-        // FIXME: Something isn't working properly in certain situations (see skipped regression tests)
-        // else if (valid_symbols[IMPLICIT_BLOCK_CLOSE]
-        //     // && lexer->lookahead == '{'
-        //     // && (!isspace(lexer->lookahead))
-        //     // && (lexer->lookahead == '{' || lexer->lookahead == '.')
-        //     && (lexer->lookahead == '|')
-        // ) {
-        //     // In case we encounter a record, record pattern or anonymous function
-        //     // we know that the current implicit block can't be closed right now
-        //     return false;
-        // }
-        // TODO: Do we need to ignore comments?
-        else if (lexer->eof(lexer)) {
-            // if (valid_symbols[IMPLICIT_EMPTY_BLOCK]) {
-            //     lexer->result_symbol = IMPLICIT_EMPTY_BLOCK;
-            //     return true;
-            // }
-            // if (valid_symbols[IMPLICIT_BLOCK_CLOSE]) {
-            //     lexer->result_symbol = IMPLICIT_BLOCK_CLOSE;
-            //     return true;
-            // }
-            // has_newline = true;
-
-
-            // TODO: No idea whether or not we'll need this in the future
-            // if (valid_symbols[TERMINATOR]
-            //     && lexer->eof(lexer)
-            //     // && scanner->blocks_to_close > 0
-            // ) {
-            //     // scanner->blocks_to_close -= 1;
-            //     // lexer->result_symbol = IMPLICIT_BLOCK_CLOSE;
-            //     lexer->result_symbol = TERMINATOR;
-            //     return true;
-            // }
-
-            break; // newline_search;
         }
         else {
             break; // newline_search;
@@ -242,15 +187,6 @@ static bool scan(Scanner* scanner, TSLexer* lexer, const bool* valid_symbols) {
     }
 
     if (has_newline) {
-        // if (valid_symbols[IMPLICIT_BLOCK_CLOSE]
-        //     // && lexer->lookahead == '{'
-        //     && (!isspace(lexer->lookahead))
-        //     // && (lexer->lookahead == '{' || lexer->lookahead == '.')
-        // ) {
-        //     // In case we encounter a record, record pattern or anonymous function
-        //     // we know that the current implicit block can't be closed right now
-        //     return false;
-        // }
         // We've seen a newline, now it's time to check, if we need to close
         // multiple blocks to get back up to the right level
         scanner->blocks_to_close = 0;
@@ -258,74 +194,15 @@ static bool scan(Scanner* scanner, TSLexer* lexer, const bool* valid_symbols) {
         // track_closed_blocks:
         while (scanner->indent_length <= *array_back(&scanner->indents)) {
             if (scanner->indent_length == *array_back(&scanner->indents)) {
-                // if (lexer->lookahead == '{' || lexer->lookahead == '.') {
-                //     break; //track_closed_blocks;
-                // }
-                // if (!has_chomped_anything) {
-                //     // Empty block on the same line
-                //     // array_pop(&scanner->indents);
-                //     scanner->blocks_to_close += 1;
-                // }
-                // scanner->blocks_to_close += 1;
-                // if (valid_symbols[IMPLICIT_BLOCK_CLOSE]
-                //     && !has_chomped_anything
-                // ) {
-                //     // Empty block at the end of a line
-                //     // scanner->blocks_to_close += 1;
-                //     return false;
-                // }
-                // if (valid_symbols[IMPLICIT_BLOCK_CLOSE]
-                //     // && valid_symbols[IMPLICIT_BLOCK_OPEN]
-                //     // && immediate_newline
-                // ) {
-                //     // array_push(&scanner->indents, lexer->get_column(lexer));
-                //     // array_pop(&scanner->indents);
-                //     // lexer->result_symbol = IMPLICIT_BLOCK_CLOSE;
-                //     // return true;
-                //     // return false;
-                //     // array_pop(&scanner->indents);
-                //     // scanner->blocks_to_close += 2;
-                //     scanner->blocks_to_close += 1;
-                //     // break; // track_closed_blocks;
-                // }
-                // array_push(&scanner->indents, lexer->get_column(lexer));
-                // scanner->blocks_to_close += 2;
-                // scanner->blocks_to_close += 1;
-                // return false;
-
-                // TODO: No idea whether or not we'll still need this in the future
-                // if (valid_symbols[IMPLICIT_EMPTY_BLOCK]
-                //     // && valid_symbols[IMPLICIT_BLOCK_CLOSE]
-                //     // && immediate_newline
-                // ) {
-                //     scanner->blocks_to_close += 1;
-                //     lexer->result_symbol = IMPLICIT_EMPTY_BLOCK;
-                //     return true;
-                // }
-
-                // if (valid_symbols[IMPLICIT_BLOCK_CLOSE]
-                //     && lexer->get_column == 0
-                // ) {
-                //     // scanner->blocks_to_close = scanner->indents.size;
-                //     scanner->blocks_to_close += 1;
-                //     // break;
-                // }
-
-                // scanner->blocks_to_close += 1;
+                // If the indentation didn't change we do nothing
                 break; // track_closed_blocks;
             }
             if (scanner->indent_length < *array_back(&scanner->indents)) {
+                // Less indentation means we need to close at least one block
                 array_pop(&scanner->indents);
                 scanner->blocks_to_close += 1;
             }
         }
-
-        // if (valid_symbols[TOPLEVEL_RESET]
-        //     && scanner->blocks_to_close == 0
-        // ) {
-        //     lexer->result_symbol = TOPLEVEL_RESET;
-        //     return true;
-        // }
 
         // Handle the first closing block now, if any, if there are more
         // they will be handled on the next scan operation because
@@ -340,24 +217,6 @@ static bool scan(Scanner* scanner, TSLexer* lexer, const bool* valid_symbols) {
             return true;
         }
     }
-    else {
-        // if (valid_symbols[IMPLICIT_EMPTY_BLOCK]
-        //     // && valid_symbols[IMPLICIT_BLOCK_CLOSE]
-        //     // && immediate_newline
-        // ) {
-        //     scanner->blocks_to_close += 1;
-        //     lexer->result_symbol = IMPLICIT_EMPTY_BLOCK;
-        //     return true;
-        // }
-        // if (valid_symbols[IMPLICIT_BLOCK_CLOSE]
-        //     && scanner->blocks_to_close > 0
-        // ) {
-        //     scanner->blocks_to_close -= 1;
-        //     lexer->result_symbol = IMPLICIT_BLOCK_CLOSE;
-        //     return true;
-        // }
-    }
-
 
     // Nothing found we can handle, let the internal lexer take over
     return false;
