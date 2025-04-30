4e23f2ab9ad6c488e138349df0355039e274cf2d
Martin Feineis <mfeineis@users.noreply.github.com>
2025-05-01 00:03:37 +0200

    feat(lsp): Add basic language server based on tower-lsp (#48)

---
+++ language-server/README.md
@@ -0,0 +1,21 @@
+# Canapea Language Server
+
+The official [Language Server Protocol](https://microsoft.github.io/language-server-protocol/) implementation of the Canapea Programming Language.
+
+## Usage
+
+*TODO:* Usage
+
+
+## Build
+
+```sh
+cargo build
+```
+
+
+## Test
+
+```sh
+cargo test
+```
---
+++ language-server/src/lib.rs
@@ -0,0 +1,68 @@
+use std::path::PathBuf;
+
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult::default())
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "server initizlized!")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct LanguageServerConfig {
+    pub transport: TransportKind,
+}
+
+// See https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#implementationConsiderations
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum TransportKind {
+    Stdio,
+    // Pipe,
+    // Socket,
+    // Nodeipc
+}
+
+#[tokio::main]
+pub async fn start(config: LanguageServerConfig) {
+    match config.transport {
+        TransportKind::Stdio => {
+            let stdin = tokio::io::stdin();
+            let stdout = tokio::io::stdout();
+
+            let (service, socket) = LspService::new(|client| Backend { client });
+            Server::new(stdin, stdout, socket).serve(service).await;
+        }
+    }
+}
+
+pub fn format_files<T: IntoIterator<Item = PathBuf>>(paths: T) {
+    for path in paths.into_iter() {
+        println!("TODO: Format file {path:#?}");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
