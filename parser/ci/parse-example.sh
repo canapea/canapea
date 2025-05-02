5ddfe98a011481fb6a8578bce8ac47c0222ee0ab
Martin Feineis <mfeineis@users.noreply.github.com>
2025-05-02 19:30:54 +0200

    chore(parser): Fix ci/parse-example.sh (#53)

--- parser/ci/parse-example.sh
+++ parser/ci/parse-example.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 
+# ci/parse-example.sh --stat --time --encoding=utf8 --no-ranges 
+
+ABI_VERSION=14
+
 run() {
   set -e
   
   echo "Generating and testing parser... PARAMS:" "$@"
-  tree-sitter generate grammar.js && tree-sitter parse "$@"
+  # tree-sitter generate grammar.js && tree-sitter parse "$@"
+
+  if ! command -v npm >/dev/null 2>&1
+  then
+    echo "NPM not found, using global tree-sitter instead"
+
+    tree-sitter generate --abi "$ABI_VERSION" --build \
+      && tree-sitter parse "$@"
+  else
+    echo "NPM found, using 'npx tree-sitter'"
+
+    npx tree-sitter generate --abi "$ABI_VERSION" --build \
+      && npx tree-sitter parse "$@"
+  fi
+
   echo "OK"
 }
 
