acce4abbb0b445230b275032ce22a791f109278f
Martin Feineis <mfeineis@users.noreply.github.com>
2025-05-05 07:27:20 +0200

    fix(parser): Parse basic example in ci/build.sh (#63)

--- parser/ci/build.sh
+++ parser/ci/build.sh
@@ -16,14 +16,16 @@ run() {
     tree-sitter generate --build --abi "$ABI_VERSION" \
       && tree-sitter test \
       && tree-sitter build "$@" \
-      && tree-sitter build --wasm "$@"
+      && tree-sitter build --wasm "$@" \
+      && tree-sitter parse --time --no-ranges --stat --quiet examples/**/*.cnp
   else
     echo "NPM found, using 'npx tree-sitter'"
 
     npx tree-sitter generate --build --abi "$ABI_VERSION" \
       && npx tree-sitter test \
       && npx tree-sitter build "$@" \
-      && npx tree-sitter build --wasm "$@"
+      && npx tree-sitter build --wasm "$@" \
+      && npx tree-sitter parse --time --no-ranges --stat --quiet examples/**/*.cnp
   fi
 
   echo "OK"
