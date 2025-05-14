eb151ea41d86a1008192dbee4fad0a7b649d30e0
Martin Feineis <mfeineis@users.noreply.github.com>
2025-05-14 06:10:32 +0200

    feat(lsp): Created preliminary semantic model (#65)

--- semantic-analyzer/src/lib.rs
+++ semantic-analyzer/src/lib.rs
@@ -4,8 +4,10 @@ mod traverse;
 use core::fmt;
 use std::collections::HashMap;
 
+use bigdecimal::BigDecimal;
 use camino::Utf8PathBuf;
 use hex;
+use num::BigInt;
 use sha2::{Digest, Sha256};
 use traverse::{Order, traverse};
 use tree_sitter::Parser;
@@ -374,138 +376,565 @@ impl Default for TreeState {
 //     module_id: ModuleId,
 // }
 
-// // TODO: Constrain what the actual possible String content can be
-// type ModulePath = String;
-// type StringContent = String;
-// type CapabilityName = String;
-// type QualifiedImportNamespace = String;
-// type CustomTypeName = String;
-// type CustomTypeConstructorName = String;
-// type TypeDeclarationName = String;
-// type FunctionName = String;
+// TODO: Constrain what the actual possible content can be
+type ModuleCanonicalName = String;
+type ModulePath = String;
+type PackageNamespace = String;
+type MultilineStringContent = String;
+type CapabilityName = String;
+type QualifiedImportNamespace = String;
+type CustomTypeName = String;
+type CustomTypeConstructorName = CustomTypeName;
+type BindingName = String;
+type FunctionName = String;
+type SimpleRecordKey = String;
+// type DontCarePattern = String;
+type StringContent = String;
+type IntType = BigInt;
+type DecimalType = BigDecimal;
+type MathsOperatorName = String;
+type RecordTypeName = String;
+type TypeVariableName = String;
+type TypeConceptName = CustomTypeName;
+type TypeConceptExpression = CustomTypeExpression;
+type TodoTopic = String;
 
-// enum ModuleId {
-//     Unknown,
-//     DevelopmentModule(ModulePath),
-//     UserModule(ModulePath),
-// }
+type AppImportDeclaration = ModuleImportDeclaration;
+type PackageImportDeclaration = ModuleImportDeclaration;
+type DevelopmentModuleImportDeclaration = ModuleImportDeclaration;
+type DevelopmentModuleExportDeclaration = ModuleExportDeclaration;
 
-// enum CompilationUnit {
-//     ModuleCompilationUnit {
-//         module_id: ModuleId,
-//         module_docs: Option<MultilineStringLiteral>,
-//         module_declaration: ModuleDeclaration,
-//         imports: Vec<ModuleImportDeclaration>,
-//         declarations: Vec<Declaration>,
-//     },
-// }
+enum ModuleId {
+    UnspecifiedModule(ModuleCanonicalName),
+    DevelopmentModule(ModuleCanonicalName, ModulePath),
+    UserModule(ModuleCanonicalName, ModulePath),
+}
 
-// struct MultilineStringLiteral {
-//     content: StringContent,
-// }
+enum PackageId {
+    Package(PackageNamespace),
+}
 
-// enum ModuleDeclaration {
-//     ApplicationDeclaration {
-//         capability_requests: Vec<CapabilityRequest>,
-//         exports: Vec<ApplicationExportDeclaration>,
-//     },
-//     DevelopmentModuleDeclaration {
-//         declared_id: ModuleId,
-//         exports: Vec<ModuleExportDeclaration>,
-//     },
-//     ModuleDeclaration {
-//         declared_id: ModuleId,
-//         exports: Vec<ModuleExportDeclaration>,
-//     },
-// }
+enum CompilationUnit {
+    AppCompilationUnit {
+        capability_requests: Vec<CapabilityRequest>,
+        imports: Vec<AppImportDeclaration>,
+        exports: Vec<AppExportDeclaration>,
+        declarations: Vec<Declaration>,
+    },
+    PackageCompilationUnit {
+        package_id: PackageId,
+        imports: Vec<PackageImportDeclaration>,
+        exports: Vec<PackageExportDeclaration>,
+        declarations: Vec<Declaration>,
+    },
+    DevelopmentModuleCompilationUnit {
+        module_id: ModuleId,
+        module_docs: ModuleDocs,
+        imports: Vec<DevelopmentModuleImportDeclaration>,
+        exports: Vec<DevelopmentModuleExportDeclaration>,
+        declarations: Vec<Declaration>,
+        development_declarations: Vec<DevelopmentDeclaration>,
+    },
+    ModuleCompilationUnit {
+        module_id: ModuleId,
+        module_docs: ModuleDocs,
+        imports: Vec<ModuleImportDeclaration>,
+        exports: Vec<ModuleExportDeclaration>,
+        declarations: Vec<Declaration>,
+    },
+}
 
-// struct CapabilityRequest {
-//     provided_by: CompilationUnit,
-//     capability: Capability,
-// }
+struct ModuleDocs {
+    content: Vec<MultilineStringContent>,
+}
 
-// enum CapabilityArg {
-//     TypeParameterArg,
-//     CustomTypeArg,
-//     StringLiteralTypeArg,
-//     IntLiteralTypeArg,
-//     DecimalLiteralTypeArg,
-// }
+struct CapabilityRequest {
+    provided_by: CompilationUnit,
+    capability: Capability,
+}
 
-// struct Capability {
-//     name: CapabilityName,
-//     params: Vec<CapabilityArg>,
-// }
+struct Capability {
+    name: CapabilityName,
+    params: Vec<CapabilityArg>,
+}
 
-// enum ApplicationExportDeclaration {
-//     FunctionApplicationExportDeclaration,
-// }
+enum CapabilityArg {
+    CustomTypeExpressionCapabilityArg(CustomTypeExpression),
+    LiteralExpressionCapabilityArg(LiteralExpression),
+}
 
-// enum ModuleExportDeclaration {
-//     FunctionModuleExportDeclaration,
-//     TypeModuleExportDeclaration,
-// }
+enum AppExportDeclaration {
+    FunctionAppExportDeclaration(BindingName),
+}
 
-// struct ModuleImportDeclaration {
-//     import_module: ModuleId,
-//     qualified_namespace: QualifiedImportNamespace,
-//     exposing: Vec<ModuleImportExposingStatement>,
-// }
+enum ModuleExportDeclaration {
+    FunctionModuleExportDeclaration(BindingName),
+    CustomTypeModuleExportDeclaration(CustomTypeName),
+}
 
-// enum ModuleImportExposingStatement {
-//     ImportExposingType {
-//         expose_as: ExposeTypeAs,
-//         constructors: Vec<ExposeTypeConstructor>,
-//     },
-// }
+enum PackageExportDeclaration {
+    FunctionPackageExportDeclaration(BindingName),
+}
 
-// enum ExposeTypeAs {
-//     ExposeTypeAsTypeName,
-//     ExposeRenamedTypeName {
-//         name: CustomTypeName,
-//     },
-// }
+struct ModuleImportDeclaration {
+    import_module: ModuleId,
+    qualified_namespace: QualifiedImportNamespace,
+    exposing: Vec<ModuleImportExposingStatement>,
+}
 
-// enum ExposeTypeConstructor {
-//     ExposeTypeConstructorName,
-//     ExposeRenamedTypeConstructor {
-//         name: CustomTypeConstructorName,
-//     },
-// }
+enum ModuleImportExposingStatement {
+    ImportExposingType {
+        expose_as: ExposeTypeAs,
+        constructors: Vec<ExposeTypeConstructorAs>,
+    },
+}
 
-// struct TypeDeclaration {
+enum ExposeTypeAs {
+    ExposeTypeAsOriginalName,
+    ExposeTypeRenamed(CustomTypeName),
+}
 
-// }
+enum ExposeTypeConstructorAs {
+    ExposeTypeConstructorAsOriginalName,
+    ExposeTypeConstructorRenamed(CustomTypeConstructorName),
+}
 
-// enum Declaration {
-//     FreeTypeDeclaration {
-//         type_annotation: TypeDeclaration,
-//     },
-//     FunctionDeclaration {
-//         type_annotation: TypeDeclaration,
-//         name: FunctionName,
-//         params: Vec<FunctionParameter>,
-//         body: BlockBody,
-//     },
-//     LetExpression,
-//     ToplevelDocs,
-//     CustomTypeDeclaration,
-//     RecordDeclaration,
-//     ExpectAssertion,
-//     ExpectTodoExpression,
-//     // TODO: Development modules only?
-//     TypeConceptDeclaration,
-//     TypeConceptInstanceDeclaration,
-//     TypeConstructorConceptDeclaration,
-// }
+enum TypeAnnotation {
+    ExplicitTypeAnnotation(ExplicitTypeAnnotation),
+    InferredTypeAnnotation(InferredTypeAnnotation),
+}
 
-// struct BlockBody {
+struct ExplicitTypeAnnotation {
+    // TODO: TypeAnnotation
+}
 
-// }
+struct InferredTypeAnnotation {
+    // TODO: TypeAnnotation
+}
 
-// struct FunctionParameter {
+enum Declaration {
+    FreeTypeDeclaration(FreeTypeDeclaration),
+    FunctionDeclaration(FunctionDeclaration),
+    LetDeclaration(LetDeclaration),
+    ToplevelDocsDeclaration {
+        content: Vec<MultilineStringContent>,
+    },
+    CustomTypeDeclaration {
+        name: CustomTypeName,
+        type_variables: Vec<TypeVariable>,
+        constructors: Vec<CustomTypeConstructorDeclaration>,
+    },
+    RecordDeclaration {
+        name: RecordTypeName,
+        type_variables: Vec<TypeVariable>,
+        record_type: RecordTypeExpression,
+    },
+    ExpectAssertionDeclaration(ExpectAssertion),
+    TodoDeclaration(TodoExpression),
+}
 
-// }
+struct FreeTypeDeclaration {
+    type_annotation: TypeAnnotation,
+}
+
+struct FunctionDeclaration {
+    type_annotation: TypeAnnotation,
+    name: FunctionName,
+    params: Vec<FunctionParameter>,
+    body: BlockBody,
+}
+
+struct LetDeclaration {
+    binding: LetExpression,
+}
+
+struct BinaryOperatorDeclaration {
+    type_annotation: TypeAnnotation,
+    name: MathsOperatorName,
+    params: Vec<FunctionParameter>,
+    body: BlockBody,
+}
+
+struct BinaryOperatorExpression {
+    left: Box<CallOrAtom>,
+    op: BinaryOperator,
+    right: Box<CallOrAtom>,
+}
+
+enum BinaryOperator {
+    PipeBinaryOperator,
+    MathsBinaryOperator(MathsOperator),
+}
+
+enum DevelopmentDeclaration {
+    TypeConceptDeclaration {
+        name: TypeConceptName,
+        type_variables: Vec<TypeVariable>,
+        requirements: TypeConceptRequirements,
+        implementations: Vec<TypeConceptImplementation>,
+    },
+    TypeConceptInstanceDeclaration {
+        concept_name: TypeConceptName,
+        instance_types: Vec<CustomTypeName>,
+        implementations: Vec<TypeConceptInstanceImplementation>,
+    },
+    TypeConstructorConceptDeclaration {
+        name: TypeConceptName,
+        type_variables: Vec<TypeConstructorConceptTypeVariable>,
+        requirements: TypeConceptRequirements,
+        implementations: Vec<TypeConstructorConceptImplementation>,
+    },
+}
+
+enum TypeConstructorConceptTypeVariable {
+    TypeVariableTypeConceptTypeVariable(TypeVariable),
+    CustomTypeNameTypeConceptVariable(CustomTypeName),
+    BuiltinTypeTypeConceptVariable(BuiltinType),
+}
+
+enum TypeConceptRequirements {
+    TypeConceptRequirements {
+        requirements: Vec<TypeConceptRequirement>,
+    },
+    TypeConceptRequirementsWithConstraints {
+        constraints: Vec<TypeConceptConstraint>,
+        requirements: Vec<TypeConceptRequirement>,
+    },
+}
+
+enum TypeConceptRequirement {
+    TypeDeclarationTypeConceptRequirement(FreeTypeDeclaration),
+}
+
+struct TypeConceptConstraint {
+    concept_name: TypeConceptName,
+    args: Vec<TypeConceptConstraintArg>,
+}
+
+enum TypeConceptConstraintArg {
+    TypeVariableTypeConceptConstraintArg(TypeVariable),
+    TypeConceptTypeConceptConstraintArg(TypeConceptExpression),
+}
+
+enum TypeConceptImplementation {
+    FunctionDeclarationTypeConceptImplementation(FunctionDeclaration),
+    LetDeclarationTypeConceptImplementation(LetDeclaration),
+    BinaryOperatorDeclarationTypeConceptImplementation(BinaryOperatorDeclaration),
+}
+
+enum TypeConstructorConceptImplementation {
+    FunctionDeclarationTypeConstructorConceptImplementation(FunctionDeclaration),
+    LetDeclarationTypeConstructorConceptImplementation(LetDeclaration),
+}
+
+enum TypeConceptInstanceImplementation {
+    FunctionDeclarationTypeConceptInstanceImplementation(FunctionDeclaration),
+    LetDeclarationTypeConceptInstanceImplementation(LetDeclaration),
+}
+
+struct TypeVariable {
+    name: TypeVariableName,
+}
+
+enum CustomTypeConstructorDeclaration {
+    CustomTypeConstructor {
+        name: CustomTypeConstructorName,
+        args: Vec<CustomTypeConstructorArg>,
+    },
+    CustomTypeConstructorWithAppliedConcepts {
+        name: CustomTypeConstructorName,
+        args: Vec<CustomTypeConstructorArg>,
+        concepts: Vec<AppliedContructorConcept>,
+    },
+}
+
+struct RecordTypeExpression {
+    entries: HashMap<SimpleRecordKey, RecordTypeEntry>,
+}
+
+enum RecordTypeEntry {
+    TypeVariableRecordTypeEntry(TypeVariable),
+    CustomTypeExpressionRecordTypeEntry(CustomTypeExpression),
+    BuiltinTypeRecordTypeEntry(BuiltinType),
+}
+
+enum BuiltinType {
+    DecimalBuiltinType(DecimalType),
+    IntBuiltinType(IntType),
+}
+
+enum CustomTypeConstructorArg {
+    CustomTypeNameConstructorArg(CustomTypeName),
+    TypeVariableConstructorArg(TypeVariable),
+    RecordTypeExpressionConstructorArg(RecordTypeExpression),
+    CustomTypeExpressionConstructorArg(CustomTypeExpression),
+    BuiltinTypeConstructorArg(BuiltinType),
+}
+
+struct CustomTypeExpression {
+    name: CustomTypeConstructorName,
+    args: Vec<CustomTypeConstructorArg>,
+}
+
+// TODO: CustomTypeTrivialValue seems silly, surely we can represent this better?
+enum AppliedContructorConcept {
+    CustomTypeTrivialValueExpressionAppliedConstructorConcept(CustomTypeTrivialValueExpression),
+    ConstructorConceptApplicationExpressionAppliedConstructorConcept(ConstructorConceptApplicationExpression),
+}
+
+struct LetExpression {
+    type_annotation: TypeAnnotation,
+    binding: LetBinding,
+    body: BlockBody,
+}
+
+struct ExpectAssertion {
+    condition: ConditionalExpression,
+}
+
+enum TodoExpression {
+    TodoExpression(TodoTopic),
+    TodoExpressionWithoutTopic(DontCarePattern),
+}
+
+// TODO: CustomType pattern in LetBinding?
+enum LetBinding {
+    BindingNameLetBinding(BindingName),
+    RecordPatternLetBinding(RecordPattern),
+    SequencePatternLetBinding(SequencePattern),
+}
+
+struct RecordPattern {
+    keys: Vec<SimpleRecordKey>,
+}
+
+struct SequencePattern {
+    items: Vec<SequencePatternItem>,
+    rest_args: Option<SequenceRestArgsPattern>,
+}
+
+enum SequencePatternItem {
+    DontCareSequencePattern(DontCarePattern),
+    BindingNameSequencePattern(BindingName),
+    LiteralExpressionSequencePattern(LiteralExpression),
+    RecordPatternSequencePattern(RecordPattern),
+    CustomTypeSequencePattern(CustomTypePattern),
+}
+
+struct SequenceRestArgsPattern {
+    name: BindingName,
+}
+
+enum CustomTypePattern {
+    TrivialCustomTypePattern {
+        constructor: CustomTypeConstructorName,
+    },
+    ComplexCustomTypePattern {
+        constructor: CustomTypeConstructorName,
+        args: Vec<CustomTypePatternArg>,
+    },
+}
+
+enum CustomTypePatternArg {
+    DontCareCustomTypePattern(DontCarePattern),
+    SequencePatternCustomTypePattern(SequencePattern),
+    RecordPatternCustomTypePattern(RecordPattern),
+    BindingNameCustomTypePattern(BindingName),
+    CustomTypePatternCustomTypePattern(CustomTypePattern),
+}
+
+struct DontCarePattern {
+}
+
+enum LiteralExpression {
+    StringLiteral {
+        content: StringContent,
+    },
+    IntLiteral {
+        content: IntType,
+    },
+    DecimalLiteral {
+        content: DecimalType,
+    },
+    // TODO: MultilineStringLiteral will be awkward in some places when it stays a LiteralExpression
+    MultilineStringLiteral {
+        content: StringContent,
+    },
+}
+
+enum FunctionParameter {
+    DontCareFunctionParameter(DontCarePattern),
+    BindingNameFunctionParameter(BindingName),
+    RecordPatternFunctionParameter(RecordPattern),
+    SequencePatternFunctionParameter(SequencePattern),
+    CustomTypePatternFunctionParameter(CustomTypePattern),
+}
+
+enum BlockBody {
+    BlockWithSingleReturn {
+        return_value: CallOrAtom,
+    },
+    BlockWithBindings {
+        bindings: Vec<BlockBinding>,
+        return_value: CallOrAtom,
+    },
+}
+
+enum BlockBinding {
+    LetExpressionBlockBinding(LetExpression),
+    ExpectAssertionBlockBinding(ExpectAssertion),
+}
+
+enum CallOrAtom {
+    CallExpressionCallOrAtom(CallExpression),
+    AtomCallOrAtom(Atom),
+}
+
+struct CallExpression {
+    target: CallTarget,
+    params: Vec<CallParameter>,
+}
+
+struct ConstructorConceptApplicationExpression {
+    target: CustomTypeName,
+    params: Vec<CallParameter>,
+}
+
+enum CallTarget {
+    ValueExpressionCallTarget(ValueExpression),
+    CustomTypeTrivialValueExpressionCallTarget(CustomTypeTrivialValueExpression),
+}
+
+enum CallParameter {
+    CallOrAtomCallParameter(CallOrAtom),
+}
+
+enum Atom {
+    AnonymousFunctionExpressionAtom(AnonymousFunctionExpression),
+    WhenExpressionAtom(WhenExpression),
+    BinaryOperatorExpressionAtom(BinaryOperatorExpression),
+    ValueExpressionAtom(ValueExpression),
+    RecordExpressionAtom(RecordExpression),
+    SequenceExpressionAtom(SequenceExpression),
+    LiteralExpressionAtom(LiteralExpression),
+    CustomTypeTrivialValueExpressionAtom(CustomTypeTrivialValueExpression),
+    TodoExpressionAtom(TodoExpression),
+}
+
+struct CustomTypeTrivialValueExpression {
+    constructor: CustomTypeConstructorName,
+}
+
+enum ValueExpression {
+    BindingNameValueExpression(BindingName),
+    QualifiedAccessValueExpression(QualifiedAccessExpression),
+}
+
+enum AnonymousFunctionExpression {
+    AnonymousFunction {
+        signature: Vec<FunctionParameter>,
+        body: Box<BlockBody>,
+    },
+    AnonymousFunctionWithoutSignature {
+        body: Box<BlockBody>,
+    },
+}
+
+enum RecordExpression {
+    RecordExpression {
+        entries: HashMap<SimpleRecordKey, CallOrAtom>,
+    },
+    RecordExpressionWithSplat {
+        splat: RecordSplatPattern,
+        entries: HashMap<SimpleRecordKey, CallOrAtom>,
+    },
+}
+
+struct RecordSplatPattern {
+    name: BindingName,
+}
+
+struct SequenceSplatPattern {
+    name: BindingName,
+}
+
+struct SequenceExpression {
+    items: Vec<SequenceExpressionEntry>,
+}
+
+enum SequenceExpressionEntry {
+    SplatSequenceExpressionEntry(SequenceSplatPattern),
+    AtomSequenceExpressionEntry(Atom),
+}
+
+struct ConditionalExpression {
+    left: CallOrAtom,
+    op: MathsOperator,
+    right: CallOrAtom,
+}
+
+struct MathsOperator {
+    name: MathsOperatorName,
+}
+
+// TODO: Model that there has to be at least one branch?
+enum WhenExpression {
+    WhenExpression {
+        subject: Box<CallOrAtom>,
+        branches: Vec<WhenBranch>,
+    },
+    WhenExpressionWithCatchAll {
+        subject: Box<CallOrAtom>,
+        branches: Vec<WhenBranch>,
+        catch_all: WhenBranchCatchAll,
+    }
+}
+
+enum WhenBranch {
+    WhenBranch {
+        pattern: WhenBranchPattern,
+        consequence: WhenBranchConsequence,
+    },
+    WhenBranchWithGuard {
+        pattern: WhenBranchPattern,
+        guard: WhenBranchGuard,
+        consequence: WhenBranchConsequence,
+    }
+}
+
+enum WhenBranchPattern {
+    RecordPatternWhenBranchPattern(RecordPattern),
+    SequencePatternWhenBranchPattern(SequencePattern),
+    LiteralExpressionWhenBranchPattern(LiteralExpression),
+    CustomTypePatternWhenBranchPattern(CustomTypePattern),
+}
+
+enum WhenBranchGuard {
+    ConditionalExpressionWhenBranchGuard(ConditionalExpression),
+}
+
+struct WhenBranchCatchAll {
+    consequence: WhenBranchConsequence,
+}
+
+struct WhenBranchConsequence {
+    consequence: Box<CallOrAtom>,
+}
+
+struct QualifiedAccessExpression {
+    target: QualifiedAccessTarget,
+    segment: QualifiedAccessSegment,
+}
+
+enum QualifiedAccessTarget {
+    RecordQualifiedAccessTarget(BindingName),
+    QualifedImportQualifiedAccessTarget(QualifiedImportNamespace)
+}
+
+// TODO: Allow "train wreck" a.b.c.d.e accessors?
+enum QualifiedAccessSegment {
+    BindingNameQualifiedAccessSegment(BindingName),
+}
 
 // impl<'a> IntoIterator for &'a mut Parent {
 //     type Item = &'a mut Child;
