6ce5274081b52089fbf0e615d059f9c3eb3ab224
Martin Feineis <mfeineis@users.noreply.github.com>
2025-07-26 12:04:48 +0200

    fix(parser): Make sure ci/playground.sh prefers a local tree-sitter via `npx` (#55)
    We really want to be sure we use the exact same tree-sitter version
    everywhere.

--- parser/ci/playground.sh
+++ parser/ci/playground.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 
+ABI_VERSION="15"
+
 run() {
   set -e
-  
+
   echo "Building parser for WASM and starting playground PARAMS:" "$@"
-  tree-sitter generate grammar.js \
-    && tree-sitter build \
-    && tree-sitter playground "$@"
-  
-  # --quiet
-  # --grammar-path
+
+  # Prefer tree-sitter that's installed via NPM so we know exactly
+  # which version we're dealing with regarding ABI incompatibilities
+  if ! command -v npm >/dev/null 2>&1
+  then
+    echo "NPM not found, using global tree-sitter instead"
+
+    tree-sitter generate --abi "$ABI_VERSION" --build \
+      && tree-sitter build \
+      && tree-sitter playground "$@"
+  else
+    echo "NPM found, using 'npx tree-sitter'"
+
+    npx tree-sitter generate --abi "$ABI_VERSION" --build \
+      && npx tree-sitter build \
+      && npx tree-sitter playground "$@"
+  fi
 
   echo "OK"
 }
